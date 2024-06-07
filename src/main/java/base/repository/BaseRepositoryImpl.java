f85da97f97e2be482b0393c5226d381bb3197b42
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-07 03:45:11 +0330

    base:BaseRepository SaveOrUpdate Generic method

---
+++ src/main/java/base/repository/BaseRepositoryImpl.java
@@ -0,0 +1,27 @@
+package base.repository;
+
+import base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.io.Serializable;
+
+
+@AllArgsConstructor
+public abstract class BaseRepositoryImpl<T extends BaseEntity<ID>, ID extends Serializable> implements BaseRepository<T, ID> {
+    private SessionFactory sessionFactory;
+
+    @Override
+    public T saveOrUpdate(T entity) {
+        Session session = sessionFactory.getCurrentSession();
+        if (entity.getId() == null)
+            session.persist(entity);
+        else
+            entity = session.merge(entity);
+        return entity;
+    }
+
+
+
+}
