2f92e5973b3e8fa21a9c7ac71d9bdccbdbb47d18
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-07 04:32:32 +0330

    Repository:technicianSubService repository find by technicianid and service id method

---
+++ src/main/java/repository/technician_subservice/TechnicianSubServiceRepositoryImpl.java
@@ -0,0 +1,39 @@
+package repository.technician_subservice;
+
+import base.repository.BaseRepositoryImpl;
+import connection.SessionFactorySingleton;
+import model.TechnicianSubService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class TechnicianSubServiceRepositoryImpl extends BaseRepositoryImpl<TechnicianSubService, Long> implements TechnicianSubServiceRepository {
+    private SessionFactory sessionFactory;
+    public TechnicianSubServiceRepositoryImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+
+    @Override
+    public Class<TechnicianSubService> getEntityClass() {
+        return TechnicianSubService.class;
+    }
+
+    @Override
+    public String getEntity() {
+        return "TechnicianSubService";
+    }
+
+    @Override
+    public TechnicianSubService find(Long technicianId, Long subServiceId) {
+        Session session = SessionFactorySingleton.getInstance().getCurrentSession();
+        Query<TechnicianSubService> query = session.createQuery("FROM model.TechnicianSubService t  WHERE t.technician.id= : technicianId AND t.subService.id =: subServiceId" , TechnicianSubService.class);
+        query.setParameter("technicianId", technicianId);
+        query.setParameter("subServiceId", subServiceId);
+        return query.getSingleResultOrNull();
+    }
+
+
+}
