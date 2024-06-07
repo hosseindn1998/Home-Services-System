447cef3add0f461232f7b8fcb629d5f7338db341
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-07 04:41:07 +0330

    Repository:order repository interface

---
+++ src/main/java/repository/Order/OrderRepository.java
@@ -0,0 +1,12 @@
+package repository.Order;
+
+import base.repository.BaseRepository;
+import model.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends BaseRepository<Order, Long> {
+    List<Order> findByCustomerId(Long customerId);
+    Optional<Order> findByCustomerIdAndId(Long customerId, Long id);
+}
