f016a98a05ca120e60225e3c4f41fa4d02e21a12
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-07 04:00:57 +0330

    Model:Create Person Entity for parent of admin technician and customer

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,38 @@
+package model;
+
+import base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@MappedSuperclass
+public class Person extends BaseEntity<Long> {
+@NotNull(message = "FirstName must not be null")
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String firstName;
+    @NotNull(message = "LastName must not be null")
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String lastName;
+    @NotNull
+    @Column(unique = true)
+    @Email(message = "Email must be valid")
+    String email;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8}$")
+    String password;
+    LocalDate registeredDate;
+    @OneToOne
+    Wallet wallet;
+}
