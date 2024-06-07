b086bcdac2837c230b69103ad9a749d4141b39bd
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-07 05:10:07 +0330

    utility:menu:private final for each service

---
+++ src/main/java/utility/Menu.java
@@ -0,0 +1,25 @@
+package utility;
+
+import service.Admin.AdminServiceImpl;
+import service.Customer.CustomerServiceImpl;
+import service.Order.OrderServiceImpl;
+import service.Service.ServiceServiceImpl;
+import service.SubService.SubServiceServiceImpl;
+import service.Technician.TechnicianServiceImpl;
+import service.Wallet.WalletServiceImpl;
+import service.technician_subservice.TechnicianSubServiceServiceImpl;
+
+import java.util.Scanner;
+
+public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+    private final AdminServiceImpl adminService = ApplicationContext.getAdminService();
+    private final CustomerServiceImpl customerService = ApplicationContext.getCustomerService();
+    private final OrderServiceImpl orderService = ApplicationContext.getOrderService();
+    private final ServiceServiceImpl serviceService = ApplicationContext.getServiceService();
+    private final SubServiceServiceImpl subServiceService = ApplicationContext.getSubServiceService();
+    private final TechnicianServiceImpl technicianService = ApplicationContext.getTechnicianService();
+    private final WalletServiceImpl walletService = ApplicationContext.getWalletService();
+    private final TechnicianSubServiceServiceImpl technicianSubServiceService = ApplicationContext.getTechnicianSubServiceService();
+    private Long loggedInUser;
+}
